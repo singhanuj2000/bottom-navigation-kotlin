c78ed5017fe339cc6f4ed8162630cb19e096f8a6
singhanuj2000 <113881629+singhanuj2000@users.noreply.github.com>
2022-09-19 11:50:32 +0530

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.bottomnav
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragmentContainerView)
+        bottomNavigation.setupWithNavController(navController)
+
+    }
+}
